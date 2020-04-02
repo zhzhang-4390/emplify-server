3fbda1361ffbf1213508903ea08407d0b6226954
Zhang Zhihao <zhzhang.4390@gmail.com>
2020-04-02 12:26:24 +0800

    Move AWS S3 IAM key into environment variables

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,6 @@ ENV PORT="80"
 ENV DB_CONNECTION="mongodb+srv://zhzhang:F7f7f7f7@emplify-4cnwj.gcp.mongodb.net/emplifydb?retryWrites=true&w=majority"
 ENV SESSION_NAME="sid"
 ENV SESSION_SECRET="May_The_Odds_Be_Ever_In_Your_Favor"
+ENV AWS_S3_ID="AKIA4NXVRQAIWA5K426J"
+ENV AWS_S3_KEY="I8nTHWdgTyIqUBuhpYtHIvZvWfbbUC/We5TvlGyd"
 CMD [ "npm", "start" ]
--- services/productService.js
+++ services/productService.js
@@ -10,8 +10,8 @@ const Product = require("../models/product");
 
 aws.config.update({ region: "ap-southeast-1" });
 const s3 = new aws.S3({
-  accessKeyId: "AKIA4NXVRQAIWA5K426J",
-  secretAccessKey: "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"
+  accessKeyId: process.env.AWS_S3_ID,
+  secretAccessKey: process.env.AWS_S3_KEY
 });
 const CLOUD_URL = "https://emplify.s3-ap-southeast-1.amazonaws.com/";
 
